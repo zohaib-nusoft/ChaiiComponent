3e48597c517fdfdf717ce8fccc5c8399b01b499f
raehan15 <raehannasir@gmail.com>
2024-08-07 11:45:51 +0500

    removed comments

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import { Link, useLocation } from "react-router-dom";
-// import classNames from "classnames";
-// import styles from "./Breadcrumb.module.scss";
-
-// const Breadcrumb: React.FC = () => {
-//   const location = useLocation();
-//   const pathSnippets = location.pathname.split("/").filter((i) => i);
-//   const buttonClass = classNames(styles["breadcrumb-wrapper"]);
-//   const breadcrumbItems = pathSnippets.map((snippet, index) => {
-//     const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
-//     const breadcrumbText = decodeURIComponent(snippet.replace(/-/g, " "));
-
-//     return (
-//       <li className={`${classNames(styles["breadcrumb-item"])}`} key={url}>
-//         <Link to={url}>{breadcrumbText}</Link>
-//       </li>
-//     );
-//   });
-
-//   return (
-//     <nav
-//       className={`${classNames(styles["breadcrumb-wrapper"])}`}
-//       aria-label="breadcrumb"
-//     >
-//       <ol className={`${classNames(styles["breadcrumb"])}`}>
-//         <li className={`${classNames(styles["breadcrumb-item"])}`}>
-//           <Link to="/">Home</Link>
-//         </li>
-//         {breadcrumbItems}
-//       </ol>
-//     </nav>
-//   );
-// };
-
-// export default Breadcrumb;
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import classNames from "classnames";
--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -1,21 +1,3 @@
-// import React from "react";
-// import type { Meta, StoryObj } from "@storybook/react";
-// import ChaiiDivider from "./Divider";
-
-// const meta: Meta<typeof ChaiiDivider> = {
-//   title: "Components/Divider",
-//   component: ChaiiDivider,
-// };
-
-// type Story = StoryObj<typeof ChaiiDivider>;
-
-// const Template: Story = {
-//   render: () => <ChaiiDivider />,
-// };
-
-// export const Default: Story = { ...Template };
-
-// export default meta;
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import ChaiiDivider from "./Divider";
