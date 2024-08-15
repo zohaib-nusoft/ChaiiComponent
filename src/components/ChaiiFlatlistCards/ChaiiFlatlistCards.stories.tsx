3550de4ca80f50cf56b9726c45cfadcee413d0d7
taha-nusoft <154229324+taha-nusoft@users.noreply.github.com>
2024-08-15 17:08:38 +0500

    Chaii Card Flatlist Component

---
+++ src/components/ChaiiFlatlistCards/ChaiiFlatlistCards.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import ChaiiFlatlistCards from ".";
+
+const meta: Meta<typeof ChaiiFlatlistCards> = {
+  title: "Components/ChaiiFlatlistCards",
+  component: ChaiiFlatlistCards,
+  argTypes: {
+    data: [
+      {
+        date: `${new Date()}`,
+        heading: "test",
+        location: "test",
+        subText: "test",
+      },
+    ],
+  },
+};
+
+type Story = StoryObj<typeof ChaiiFlatlistCards>;
+
+const Template: Story = {
+  render: (args) => <ChaiiFlatlistCards {...args} />,
+};
+
+export const Default: Story = {
+  ...Template,
+  args: {
+    data: [
+      {
+        date: `${new Date()}`,
+        heading: "test",
+        location: "test",
+        subText: "test",
+      },
+    ],
+  },
+};
+
+export default meta;
---
+++ src/components/ChaiiFlatlistCards/index.tsx
@@ -0,0 +1,70 @@
+import { Col, Flex, Row } from "antd";
+import classNames from "classnames";
+import React from "react";
+import { Button, ChaiiText } from "../..";
+import styles from "./CardStyles.module.scss";
+
+interface props {
+  data: {
+    id?: string;
+    date: string;
+    location: string;
+    heading: string;
+    subText: string;
+    buttons?: { logo: React.ReactNode }[];
+  }[];
+}
+
+const ChaiiFlatlistCards = ({ data }: props) => {
+  return (
+    <Flex className="w-100">
+      <Row gutter={[20, 20]} className="w-100">
+        {data?.map((i) => (
+          <Col span={12}>
+            <Row className="w-100">
+              <Col span={18}>
+                <Col className="pb-1">
+                  <ChaiiText
+                    className={classNames(styles.experiance_card_title)}
+                  >
+                    {i?.heading}
+                  </ChaiiText>
+                </Col>
+                <Col>
+                  <ChaiiText
+                    className={classNames(styles.experiance_card_company_name)}
+                  >
+                    {i?.subText}
+                  </ChaiiText>
+                </Col>
+                <Col>
+                  {i?.date && i.location ? (
+                    <ChaiiText className={styles.experiance_card_location}>
+                      {i?.location} - {i?.date}
+                    </ChaiiText>
+                  ) : (
+                    <></>
+                  )}
+                </Col>
+              </Col>
+              <Col
+                className="d-flex gap-2 justify-content-end align-items-start"
+                span={6}
+              >
+                {i?.buttons ? (
+                  i.buttons.map(({ logo }) => (
+                    <Button icon={logo} btnClass="iconBtnCircle" />
+                  ))
+                ) : (
+                  <></>
+                )}
+              </Col>
+            </Row>
+          </Col>
+        ))}
+      </Row>
+    </Flex>
+  );
+};
+
+export default ChaiiFlatlistCards;
