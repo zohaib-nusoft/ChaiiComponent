c9b05c0b17427e87447c8e1931840cf83ea62697
Taha <taha@nusoft.co>
2024-09-17 06:38:50 -0700

    removed commented code

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -34,36 +34,6 @@ interface props {
 }
 
 const LineChart = ({ data }: props) => {
-  // Use months as labels
-  const monthLabels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-  ];
-
-  // const data = {
-  //   labels: monthLabels,
-  //   datasets: [
-  //     {
-  //       data: [
-  //         { x: "January", y: 33, customData: { python: 3, js: 30 } },
-  //         { x: "February", y: 53, customData: { python: 10, js: 40 } },
-  //         { x: "March", y: 85, customData: { python: 15, js: 70 } },
-  //         { x: "April", y: 41, customData: { python: 20, js: 21 } },
-  //         { x: "May", y: 44, customData: { python: 25, js: 19 } },
-  //         { x: "June", y: 65, customData: { python: 30, js: 35 } },
-  //       ],
-  //       fill: 1,
-  //       backgroundColor: "rgba(75,192,192,0.2)",
-  //       borderColor: "rgba(75,192,192,1)",
-  //     },
-  //   ],
-  // };
-
   const options: any = {
     scales: {
       x: {
